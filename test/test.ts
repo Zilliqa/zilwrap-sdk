1b4344d95f8224cd8cb1e1bcfd9c05d36b7fa8a2
teye <teye@zilliqa.com>
2021-03-04 11:06:17 +0800

    update tests

--- test/test.ts
+++ test/test.ts
@@ -1,23 +1,47 @@
 var assert = require('assert');
+const { Network } = require('../lib/constants');
+const Zilwrap = require('../lib/index').Zilwrap;
 const fs = require('fs');
 const ZilTest = require('zilliqa-testing-library').default;
+const Test = new ZilTest();
+
+before(async () => {
+    console.log("setting up pre-test stuff...");
+    const contract = fs.readFileSync(__dirname + "/fixtures/token_contract.scilla", 'utf-8');
+
+    console.log("generating accounts...");
+    await Test.generateAccounts(3);
+    assert(Test.accounts.length === 3);
+
+    console.log("loading contract: %o", __dirname + "/fixtures/token_contract.scilla");
+    await Test.loadContract(contract);
+    assert(Test.contracts.length === 1);
+
+});
 
 describe('Zilwrap-testing', () => {
-    const contract = fs.readFileSync(__dirname + "/fixtures/token_contract.scilla", 'utf-8');
-    const Test = new ZilTest();
-    let contractAddress;
+    let deployedAddress = "";
 
-    it('should generate 3 accounts', async () => {
-        await Test.generateAccounts(3);
-        assert(Test.accounts.length === 3);
-    });
+    // it('should deploy token contract', async () => {
+    //     const preparedContract = Test.contracts[0];
+        
+    //     const [tx, deployed] = await preparedContract.deploy(
+    //         Test.accounts[0].address,
+    //         {
+    //             contract_owner: Test.accounts[0].address,
+    //             name: "ACME-TOKEN",
+    //             symbol: "ACME",
+    //             decimals: "12",
+    //             init_supply: "0",
+    //         }
+    //     );
 
-    it('should load contract into Testing Suite and run scilla checker', async () => {
-        await Test.loadContract(contract);
-        assert(Test.contracts.length === 1);
-    });
+    //     assert(tx.receipt.success === true, "Transaction failed");
 
-    it('should deploy token contract', async () => {
+    //     deployedAddress = deployed.address;
+    // });
+
+    beforeEach(async () => {
         const preparedContract = Test.contracts[0];
         
         const [tx, deployed] = await preparedContract.deploy(
@@ -32,16 +56,67 @@ describe('Zilwrap-testing', () => {
         );
 
         assert(tx.receipt.success === true, "Transaction failed");
+
+        deployedAddress = deployed.address;
+    });
+
+    it('should wrap $ZIL to token', async () => {
+        const settings = {
+            contractAddress: deployedAddress
+        };
+        const zilwrap = new Zilwrap(Network.Isolated, Test.accounts[0].privateKey, settings);
+        await zilwrap.init();
+        const result = await zilwrap.wrap('10'); // wrap 10 $ZIL
+
+        const expectedResult = JSON.parse(`
+        [
+            {
+                "_eventname": "Minted",
+                "address": "${deployedAddress.toLowerCase()}",
+                "params": [
+                    {
+                        "type": "ByStr20",
+                        "value": "${Test.accounts[0].address.toLowerCase()}",
+                        "vname": "minter"
+                    },
+                    {
+                        "type": "ByStr20",
+                        "value": "${Test.accounts[0].address.toLowerCase()}",
+                        "vname": "recipient"
+                    },
+                    {
+                        "type": "Uint128",
+                        "value": "10000000000000",
+                        "vname": "amount"
+                    }
+                ]
+            }
+        ]
+        `);
+
+        const deployed = Test.deployedContracts[deployedAddress];
+        const state = await deployed.getState();
+
+        // should have 10000000000000 token = 10$ZIL
+        assert(state.balances[Test.accounts[0].address.toLowerCase()] === '10000000000000');
+        
+        // compare event logs
+        assert(JSON.stringify(result.event_logs) === JSON.stringify(expectedResult));
+    });
+
+    it('should return error when wrap more $ZIL than current balance', async () => {
+        const settings = {
+            contractAddress: deployedAddress
+        };
+        const zilwrap = new Zilwrap(Network.Isolated, Test.accounts[0].privateKey, settings);
+        await zilwrap.init();
+        assert.rejects(async () => await zilwrap.wrap('9999'), Error, 'Insufficient $ZIL balance')
     });
 
     // deploy token contract
     // beforeEach(() => {
 
     // });
-
-    it('should return -1 when the value is not present', () => {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-    });
 });
 
 // remove test.js
